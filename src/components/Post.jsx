d7f8d3360be2729cc35ad58f300ffb8014e8204a
Jefferson Clementino Ferreira <jefferson.ferreira@mjv.com.br>
2024-07-30 16:23:49 -0300

    Estrutura da aplicação - Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,19 @@ export function Post(props) {
                     <a href="#">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+                <footer>
+                    <button type="submit">
+                        Publicar
+                    </button>
+                </footer>
+            </form>
         </article>
     )
 }
