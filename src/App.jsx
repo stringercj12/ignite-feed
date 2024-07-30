ee18761c912008aabbdc662c6d66eebebfca6e01
Jefferson Clementino Ferreira <jefferson.ferreira@mjv.com.br>
2024-07-30 12:46:07 -0300

    Estrutura da aplicação - CSS global

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './styles.css'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+
+
+      
+    </div>
+  )
+}
+
+export default App
