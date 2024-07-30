9179c302c14c8ad94a6703b02feffa22b0e08bb8
Jefferson Clementino Ferreira <jefferson.ferreira@mjv.com.br>
2024-07-30 15:51:03 -0300

    Estrutura da aplicação - Componente: Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,13 +7,27 @@ import styles from './Post.module.css';
 
 export function Post(props) {
     return (
-        <div>
-            <h1>
-                {props.author}
-            </h1>
-            <p>
-                {props.content}
-            </p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/stringercj12.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Jefferson Ferreira</strong>
+                        <span>
+                            Web Developer
+                        </span>
+                    </div>
+                </div>
+
+                <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p> Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="#">jane.design/doctorcare</a></p>
+                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
     )
 }
